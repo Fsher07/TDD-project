b302de232d8abd6d03c166b7823c75635d691805
Fsher07 <furkansalimm@gmail.com>
2022-06-30 19:36:08 +0300

    Add an algorithm for factorial

--- solver.rb
+++ solver.rb
@@ -1,5 +1,12 @@
 class Solver
   def factorial(int)
     raise "Please enter a number which is equal or greater than zero" if int < 0
+
+    multiply = 1
+    while int.positive?
+      multiply *= int
+      int -= 1
+    end
+    multiply
   end
 end
