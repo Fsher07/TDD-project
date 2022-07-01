6a94afc62b4cf8ad1996fc965b810cc8e284eae0
Fsher07 <furkansalimm@gmail.com>
2022-07-01 13:44:54 +0300

    Fix linters

--- solver.rb
+++ solver.rb
@@ -26,4 +26,3 @@ class Solver
     end
   end
 end
-
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -8,7 +8,7 @@ describe Solver do
   end
 
   it 'when the argument is 0 return 1' do
-    expect(solver.factorial(0)). to eq 1
+    expect(solver.factorial(0)).to eq 1
   end
 
   it 'raise an exception when a negative integer is entered' do
